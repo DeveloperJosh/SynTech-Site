766442f9a15e9629ad9f36ad57fd966268956995
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-20 21:20:55 -0400

    yes

--- restapi.js
+++ restapi.js
@@ -2,26 +2,29 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/info', function (req, res) {
+app.get('/data', function (req, res) {
     var data = fs.readFileSync("data.json");
     var info = JSON.parse(data);
     res.send(info);
-})
-
-app.get('/download', function (req, res) {
-    //download file
-    res.download('download/Program.exe');
 })
 
 app.get('/', function (req, res) {
     res.send('Hello This is the Syn Rest API');
 })
 
-app.get('/api/v1/users', function (req, res) {
+app.get('/users', function (req, res) {
     //get all users
     var data = fs.readFileSync("data.json");
     var info = JSON.parse(data);
     res.send(info.users);
+})
+
+app.get('/image', function (req, res) {
+    images = [
+        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://blue.is-from.space/chrome_y3BJ1W6xd7.png",
+    ]
+    res.send(images[Math.floor(Math.random() * images.length)]);
 })
 
 app.listen(process.env.PORT || 3000, function(){
