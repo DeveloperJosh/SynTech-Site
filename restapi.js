74c8cad6f3bd67f14c084d2c0f55c36f03699105
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-20 21:24:52 -0400

    code

--- restapi.js
+++ restapi.js
@@ -20,11 +20,11 @@ app.get('/users', function (req, res) {
 })
 
 app.get('/image', function (req, res) {
-    images = [
-        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-        "https://blue.is-from.space/chrome_y3BJ1W6xd7.png",
-    ]
-    res.send(images[Math.floor(Math.random() * images.length)]);
+    //random image
+    var data = fs.readFileSync("data.json");
+    var info = JSON.parse(data);
+    var random = Math.floor(Math.random() * info.images.length);
+    res.send(info.images[random]);
 })
 
 app.listen(process.env.PORT || 3000, function(){
