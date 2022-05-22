70b72be99608f1a48bd943c34b2b332416df14ea
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-22 15:20:29 -0400

    updates

--- rest-api.js
+++ rest-api.js
@@ -10,42 +10,23 @@ app.get('/', function (req, res) {
     res.json({
         message: 'No API endpoint specified'
     });
+});
 
-})
-
-app.param('city', function(req, res, next, city) {
+app.get('/api/weather/:city', function(req, res) {
     const options = {
         method: 'GET',
         headers: {
             'X-RapidAPI-Host': 'community-open-weather-map.p.rapidapi.com',
             'X-RapidAPI-Key': process.env.API_KEY
-        }
-    };
-    
-    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${city}`, options)
+        }};
+    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?q=${req.params.city}`, options)
     .then(response => response.json())
     .then(data => {
-        res.send(data);
+        res.json(data);
     })
-  });
-  
-app.get('/api/weather/:city', function(req, res) {
-    res.send(req.city);
-
-});   
-
-app.get('/api/porn', function (req, res) {
-    fetch('https://nekobot.xyz/api/image?type=blowjob')
-    .then(response => response.json())
-    .then(data => {
-        const image = data['message']
-        res.json({
-            "url": image
-        }) 
-    })
-  });
+});
 
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
