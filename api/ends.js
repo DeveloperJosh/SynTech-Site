fab0de387734da40c94f98a2ca0ff74d07c8c1ab
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-06-09 15:14:07 -0400

    yes

--- api/ends.js
+++ api/ends.js
@@ -4,40 +4,21 @@ const path = require('path');
 const app = express();
 const rateLimit  = require('express-rate-limit');
 const find = require('../functions/index');
+require('dotenv').config();
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     message: 'Too many requests from this IP, please try again after an hour',
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
-function premium(ip) {
-    /// vpn ids
-    premium_ip = ['89.238.142.238', '::1']
-    if (ip === premium_ip) {
-        return true
-    }
-    return false
-}
-
-check_premium = (req, res, next) => {
-    if (premium(req.ip)) {
-        next()
-    } else {
-        res.status(403).send('Forbidden')
-    }
-}
-
 app.use(express.static(__dirname + '/public'));
 
 api.get('/', function(req, res) {
+    /// check if the ip is on lmitlist
     res.send({ message: 'Welcome to the API', version: '1.0.0', ip: `Here is the request ip ${req.ip}` });
-});
-
-api.get('/premium', check_premium, function(req, res) {
-    res.send({ message: 'You are a premium user' });
 });
 
 api.get('/image', limiter, function(req, res) {
